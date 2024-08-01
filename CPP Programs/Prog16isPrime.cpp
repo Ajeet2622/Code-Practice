ae1d5f6277cc9fa429c569b10c9de7fa73d30101
Ajeet2622 <ajeetrathourr@gmail.com>
2024-08-01 13:08:52 +0530

    Login change in loof to decrease numbner of steps(sqrt(n))

--- CPP Programs/Prog16isPrime.cpp
+++ CPP Programs/Prog16isPrime.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+bool isprime(int a);
 int main(){
-	
+	int n = 51;
+	if(isprime(n)){
+		cout<<"prime";
+	}else{
+		cout<<"Not prime";
+	}
+
 	return 0;
+}
+bool isprime(int num){
+	for(int i = 2 ; i<num/2;i++){
+		if(num%i==0){
+			return false;
+		}
+	}
+	return true;
 }
