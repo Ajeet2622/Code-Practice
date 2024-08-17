991d5697b3e735cd700517bfe77283da4094383e
Ajeet2622 <ajeetrathourr@gmail.com>
2024-08-17 19:37:52 +0530

    p01singlly insertafter given Position Node

---
+++ Linked List/p01singlly insertafter g Pos Node.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+	int data;
+	Node* next;
+	Node(int Data){
+		data = Data;
+		next = nullptr;
+
+	}
+};
+
+Node* insertAtPos(Node* head,int pos,int data){
+	if(head==nullptr)
+	return head;
+	Node* new_node = new Node(data);
+	if(pos == 1){
+		new_node->next = head;
+		head = new_node;
+	}
+	Node* curr = head;
+	int i=1;
+	while (i<pos-1)
+	{
+		if(curr==nullptr){
+			cout<<"Invalid Position "<<curr->data;
+			return head;
+		}
+		i++;
+		curr = curr->next;
+		/* code */
+	}
+	// if(curr=nu)
+
+	new_node->next = curr->next;
+	curr->next = new_node;
+	return head;
+}
+
+void printList(Node* node){
+	while(node!=nullptr){
+		
+		cout<<node->data<<" ";
+		node = node->next;
+	}
+}
+int main(){
+	Node* head = new Node(5);
+	head->next = new Node(8);
+	head->next->next = new Node(6);
+	head->next->next->next = new Node(12);
+	head->next->next->next->next = new Node(7);
+	head->next->next->next->next->next = new Node(3);
+    printList(head);
+	cout<<endl;
+	head = insertAtPos(head,3,78);
+	printList(head);
+
+	return 0;
+}
